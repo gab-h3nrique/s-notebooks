1f0694646729e9c09734c53baafc820f91ebd759
Gabriel Henrique <gabrielbielrique10@gmail.com>
2022-11-03 21:34:57 -0300

    finalizando modal de os

--- src/components/modals/ModalComponent.tsx
+++ src/components/modals/ModalComponent.tsx
@@ -5,15 +5,16 @@
 
 export interface Props {
     close: any;
+    open: boolean;
     className: string;
     children: React.ReactNode;
 }
 
-const ModalComponent = ({close, className, children}:Props) => {
+const ModalComponent = ({close,open, className, children}:Props) => {
     return (
         <>
-            <div onClick={close} className="fixed bg-slate-900 opacity-40 top-0 left-0 right-0 bottom-0 z-40 "></div>
-            <div className={`fixed top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] ${className} z-50`}>
+            <div onClick={close} className={`fixed bg-slate-900 opacity-40 top-0 left-0 right-0 bottom-0 z-40 ${!open ? "opacity-0 pointer-events-none" : "opacity-1 pointer-events-auto"} duration-150`}></div>
+            <div className={`fixed top-[49%] left-2/4 translate-x-[-50%] translate-y-[-50%] z-50 ${className} ${!open ? "opacity-0 scale-50 pointer-events-none" : "opacity-1 scale-[.99] pointer-events-auto"} hover:scale-100 duration-300`} >
                 {children}
             </div>
         </>
--- src/components/modals/NewOrderModal.tsx
+++ src/components/modals/NewOrderModal.tsx
@@ -5,6 +5,7 @@ import { Key, ReactElement, ReactNode, useEffect, useState } from "react";
 import ReactDom from "react-dom";
 import BadgeCheckIcon from "../icons/BadgeCheckIcon";
 import CheckDouble from "../icons/CheckDouble";
+import CheckIcon from "../icons/CheckIcon";
 import CircleCheckIcon from "../icons/CircleCheckIcon";
 import CircleIcon from "../icons/CircleIcon";
 import CloseIcon from "../icons/CloseIcon";
@@ -32,14 +33,17 @@ type Checkbox = {
 }
 
 const NewOrderModal = ({isOpen, onClose, formOder}:Props) => {
+    
     const [info , setInfo] = useState<Checkbox>({carregador: false, bateria: false, cordaDeForca: false, bolsa: false})
-    if(!isOpen) return null
+    const [dropdown, setDropdown] = useState<boolean>(false)
+    const [tec, setTec] = useState<string>("")
+    // if(!isOpen) return null
 
     return ReactDom.createPortal(
         <>
-            <ModalComponent close={onClose} className={`opacity-100 duration-100`}>
+            <ModalComponent close={onClose} open={isOpen} className={`opacity-100 duration-100`}>
                 {/* <div className="flex flex-col bg-gray-200 w-[32rem] h-[50rem] rounded-2xl p-4 gap-4"> */}
-                <div className="flex flex-col bg-slate-100 w-[36rem] h-fit rounded-2xl py-4 gap-4">
+                <div className="flex flex-col bg-slate-100 w-[36rem] h-fit scale-[.93] rounded-2xl py-4 gap-4">
                     <header className="px-4">
                         <section className="flex w-full justify-between">
                             <div className="flex justify-center items-center">
@@ -127,10 +131,36 @@ const NewOrderModal = ({isOpen, onClose, formOder}:Props) => {
                             
                             <div className="grid gap-2 grid-cols-2">
     
-                                <div >
-                                        <label className="block text-sm font-medium text-slate-500">Nome do equipamento</label>
+                                {/* <div >
+                                        <label className="block text-sm font-medium text-slate-500">Tipo de equipamento</label>
                                         <input type="text"  className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" placeholder="" required/>
-                                </div> 
+                                </div>  */}
+                                {/* ___________________________________________________________________________________ */}
+
+                                <div>
+                                    <div onClick={()=> setDropdown(!dropdown)}>
+                                        <label className="block text-sm font-medium text-slate-500">Tipo de equipamento</label>
+                                        <button type="button" className="flex justify-between px-5 text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150">
+                                        <label className="block text-sm font-medium text-slate-500"></label>
+                                        
+                                        <svg className={`-mr-1 ml-2 h-5 w-5 ${dropdown ? "rotate-[-180deg]" : "rotate-[0deg]"} duration-150`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" >
+                                            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" />
+                                        </svg>
+                                        </button>
+                                    </div>
+
+                                    <div className={`${!dropdown ? "opacity-0 pointer-events-none" : "opacity-1 pointer-events-auto"} duration-150 absolute  left-10 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg`} >
+                                        <div className="py-1" >
+
+                                            <a onClick={()=>{setDropdown(!dropdown); console.log('tste')}} className=" block px-4 py-2 text-sm font-medium text-slate-500 hover:bg-slate-100 hover:scale-105 duration-150 cursor-pointer" >Notebook</a>
+                                            <a onClick={()=>{setDropdown(!dropdown); console.log('tste')}} className=" block px-4 py-2 text-sm font-medium text-slate-500 hover:bg-slate-100 hover:scale-105 duration-150 cursor-pointer" >Desktop</a>
+                                            <a onClick={()=>{setDropdown(!dropdown); console.log('tste')}} className=" block px-4 py-2 text-sm font-medium text-slate-500 hover:bg-slate-100 hover:scale-105 duration-150 cursor-pointer" >Outros</a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* ___________________________________________________________________________________ */}
+                                
                                 <div >
                                         <label className="block text-sm font-medium text-slate-500">Número de série</label>
                                         <input type="text"  className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" placeholder="" required/>
@@ -201,47 +231,96 @@ const NewOrderModal = ({isOpen, onClose, formOder}:Props) => {
                             
                             <div className="flex flex-col">
 
-                                    <article className="grid gap-2 grid-cols-8 py-1">
+                                <article className="grid gap-2 grid-cols-8 py-1">
 
-                                        <div className="col-span-2 flex flex-col gap-1">
-                                            <label className="block text-sm font-medium text-slate-500">Backup</label>
-                                            <div onClick={()=>setInfo({...info, carregador: !info.carregador})} className={`flex items-center ${info.carregador ? 'bg-orange-100' : 'bg-slate-100' } gap-1 px-1 py-2  rounded-lg w-full hover:scale-110 duration-100 cursor-pointer `}>
-                                                {info.carregador ? <CircleCheckIcon  width={20} height={20} fill={`#F06531`} />
-                                                        : <CircleIcon  width={20} height={20} fill={`#94a3b8`} />}
-                                                <label  className={`text-sm text-slate-500  ${info.carregador ? 'text-orange-500' : 'text-slate-500' } font-semibold cursor-pointer`}>Fazer backup</label>
+                                    <div className="col-span-2 flex flex-col gap-1">
+                                        <label className="block text-sm font-medium text-slate-500"></label>
+                                        <div onClick={()=>setInfo({...info, carregador: !info.carregador})} className={`flex items-center ${info.carregador ? 'bg-orange-100' : 'bg-slate-100' } gap-1 px-1 py-2  rounded-lg w-full hover:scale-110 duration-100 cursor-pointer `}>
+                                            {info.carregador ? <CircleCheckIcon  width={20} height={20} fill={`#F06531`} />
+                                                    : <CircleIcon  width={20} height={20} fill={`#94a3b8`} />}
+                                            <label  className={`text-sm text-slate-500  ${info.carregador ? 'text-orange-500' : 'text-slate-500' } font-semibold cursor-pointer`}>Fazer backup</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-span-6 flex items-end py-[2.1px]">
+                                        {/* <label className="block text-sm font-medium text-slate-500">&nbsp;</label> */}
+                                        <input type="text"  className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" placeholder="Descrição do backup"/>
+                                    </div>
+
+                                    
+
+                                </article>
+                                <article className="grid grid-cols-2 gap-x-2">
+
+                                    <div>
+                                        <label className="block text-sm font-medium text-slate-500">Relatório do cliente</label>
+                                        <textarea className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" />
+                                    </div> 
+
+                                    <div >
+                                        <label className="block text-sm font-medium text-slate-500">Laudo técnico</label>
+                                        <textarea className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" />
+                                    </div> 
+
+                                    <div className="col-span-2">
+                                        <label className="block text-sm font-medium text-slate-500">Serviços</label>
+                                        <textarea className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" />
+                                    </div>
+
+                                </article>
+
+                                <article className="flex justify-between gap-2">
+                                   
+                                    
+
+                                    <div className="w-full">
+                                        <div >
+                                            <label className="block text-sm font-medium text-slate-500">Tipo de equipamento</label>
+                                            <button type="button" onClick={()=> setDropdown(!dropdown)} className="flex justify-between px-5 text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150">
+                                            <label className="block text-sm font-medium text-slate-500">{tec ? tec : "Técnico responsável"}</label>
+                                            
+                                            <svg className={`-mr-1 ml-2 h-5 w-5 ${dropdown ? "rotate-[-180deg]" : "rotate-[0deg]"} duration-150`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" >
+                                                <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" />
+                                            </svg>
+                                            </button>
+                                        </div>
+
+                                        <div className={`${!dropdown ? "opacity-0 pointer-events-none" : "opacity-1 pointer-events-auto"} duration-150 absolute  right-16 bottom-2 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg`} >
+                                            <div className="py-1" >
+
+                                                <a onClick={()=>{setDropdown(!dropdown); setTec("Gabriel")}} className=" block px-4 py-2 text-sm font-medium text-slate-500 hover:bg-slate-100 hover:scale-105 duration-150 cursor-pointer rounded-lg" >Gabriel</a>
+                                                <a onClick={()=>{setDropdown(!dropdown); setTec("Henrique")}} className=" block px-4 py-2 text-sm font-medium text-slate-500 hover:bg-slate-100 hover:scale-105 duration-150 cursor-pointer rounded-lg" >Henrique</a>
+                                                <a onClick={()=>{setDropdown(!dropdown); setTec("Vieira")}} className=" block px-4 py-2 text-sm font-medium text-slate-500 hover:bg-slate-100 hover:scale-105 duration-150 cursor-pointer rounded-lg" >Vieira</a>
+
                                             </div>
                                         </div>
-                                        <div className="col-span-6 flex items-end py-[2.1px]">
-                                            {/* <label className="block text-sm font-medium text-slate-500">&nbsp;</label> */}
-                                            <input type="text"  className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" placeholder="Descrição do backup" required/>
-                                        </div>
-
-                                        
-
-                                    </article>
-
-                                    <div className="col-span-8">
-                                            <label className="block text-sm font-medium text-slate-500">Relatório do cliente</label>
-                                            <textarea className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" placeholder="" required/>
-                                        </div> 
-
-                                        <div className="col-span-8">
-                                            <label className="block text-sm font-medium text-slate-500">Laudo técnico</label>
-                                            <textarea className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" placeholder="" required/>
-                                        </div> 
-
-                                        <div className="col-span-8">
-                                            <label className="block text-sm font-medium text-slate-500">Serviços</label>
-                                            <textarea className="text-sm font-medium text-slate-600 rounded-lg w-full bg-gray-50 p-1 border-2 border-gray-300 outline-none focus:border-transparent focus:ring focus:ring-orange-400 hover:scale-y-105 duration-150" placeholder="" required/>
-                                        </div> 
+                                    </div>
                                     
+
+                                    <div className="w-full flex items-end py-[2.1px]">
+                                        {/* <label className="block text-sm font-medium text-slate-500">&nbsp;</label> */}
+                                        <div onClick={()=>setInfo({...info, carregador: !info.carregador})} className={`flex items-center ${info.carregador ? 'bg-orange-100' : 'bg-slate-100' } gap-1 px-1 py-2  rounded-lg w-full hover:scale-105 duration-150 cursor-pointer `}>
+                                            {info.carregador ? <CircleCheckIcon  width={20} height={20} fill={`#F06531`} />
+                                                    : <CircleIcon  width={20} height={20} fill={`#94a3b8`} />}
+                                            <label  className={`text-sm text-slate-500  ${info.carregador ? 'text-orange-500' : 'text-slate-500' } font-semibold cursor-pointer`}>Confirmação de entrega</label>
+                                        </div>
+                                    </div>
+                                  
+
+                                </article>
             
-                                </div>
+                            </div>
 
                         </section>
                     </main>
-                    <footer>
-
+                    <footer className="flex justify-center items-center">
+                        <div onClick={() => console.log('sldkfj')} className="flex w-full px-6">
+                            <div className={`flex justify-center items-center bg-orange-500 gap-2 cursor-pointer w-full rounded-2xl py-3 px-3 duration-150 hover:scale-y-110`}>
+                                    <CheckIcon width={22} height={22} fill={`white`}/>
+                                <p className={`text-white text-sm font-semibold duration-500`}>
+                                    Salvar ordem de serviço
+                                </p>
+                            </div>
+                        </div>
                     </footer>
                 </div>
             </ModalComponent>
