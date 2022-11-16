82965811eef297f9ac4ecdb2d794ac0211087191
Gabriel Henrique <gabrielbielrique10@gmail.com>
2022-11-15 22:05:48 -0300

    aplicando configuracao final do pdf

--- src/pages/orderPdf.tsx
+++ src/pages/orderPdf.tsx
@@ -2,55 +2,38 @@ import type { GetServerSideProps, GetStaticProps, NextPage } from 'next'
 import { useContext, useEffect, useState } from 'react';
 import Api from '../../lib/api';
 
-import Router from 'next/router';
-
-import LoginForm from '../components/LoginForm';
-import { AuthContext } from '../context/auth';
-
 interface Props {
   content: string;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { id } = context.query
+  const { host:baseUrl } = context.req.headers
 
-
-  // const {response} = await Api.get('/api/auth/orders', {id:id})
-
-
+  const {response} = await Api.get(`http://${baseUrl}/api/teste`, {id:id})
 
   return { 
-    // props: { content: response ? JSON.parse(response) : null }
-    props: { content: 'a' }
+    props: { content: response }
   }
   
 }
 
 const Home: NextPage<Props> = ({content}) => {
 
-  // const { id } = props
-
   const [order, setOrder] = useState<any>();
 
-  // const getOrder = async () => {
-  //   const {response} = await Api.get('/api/auth/orders', {id:38})
-  //   setOrder(response)
-  // }
-
   useEffect(()=>{
-    setOrder(content)
-    // (async () => {
-    //   setOrder(response)
-    //   await getOrder()
-    // })()
+    (async () => {
+      setOrder(content)
+    })()
   },[])
 
 return (
   <>
-      <div>{order}</div>
-      <div>{baseUrl}</div>
+      <main className="flex overflow-auto flex-col gap-4 px-4 bg-white w-[210mm] h-[297mm] m-auto mt-10" >{order?.status}</main>
   </>
 )
 }
 
 export default Home
+
