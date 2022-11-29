b721cb309e6b574a6cdb095c8abee577f8f095a0
Gabriel Henrique <gabrielbielrique10@gmail.com>
2022-11-28 21:24:49 -0300

    aplicando correção pdf

--- src/pages/api/auth/pdf.ts
+++ src/pages/api/auth/pdf.ts
@@ -2,7 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import puppeteer from 'puppeteer'
 
+import chromium from "chrome-aws-lambda";
+import playwright from "playwright-core";
 
+// https://www.umuttufanoglu.dev/blog/puppeteer-nextjs
 export default async function handler( req: NextApiRequest,res: NextApiResponse<any>) {
     
     const { host } = req.headers
@@ -12,50 +15,6 @@ export default async function handler( req: NextApiRequest,res: NextApiResponse<
 
     const cookie = req.headers.cookie
     const token = <string> cookie?.replace('auth=', 'Bearer ');
-
-
-    (async () => {
-        const PCR = require("puppeteer-chromium-resolver");
-        const option = {
-            revision: "",
-            detectionPath: "",
-            folderName: ".chromium-browser-snapshots",
-            defaultHosts: ["https://storage.googleapis.com", "https://npm.taobao.org/mirrors"],
-            hosts: [],
-            cacheRevisions: 2,
-            retry: 3,
-            silent: false
-        };
-
-        const locateChrome = require('locate-chrome');
-        const executablePath = await new Promise(resolve => locateChrome((arg: any) => resolve(arg)));
-
-        const stats = await PCR(option);
-        const browser = await stats.puppeteer.launch({
-            headless: false,
-            args: ["--no-sandbox"],
-            executablePath: executablePath
-        }).catch(function(error:any) {
-            console.log(error);
-        });
-
-
-
-        const page = await browser.newPage();
-        page.setExtraHTTPHeaders({
-            Authorization: token
-        })
-        await page.goto(`http://${host}/orderPdf?id=${id}`, {  waitUntil: 'networkidle0' })
-        const pdf = await page.pdf({
-            printBackground: true,
-            format: 'Letter',
-        })
-        await browser.close();
-
-        return res.setHeader("content-type", "application/pdf").send(pdf)
-
-
-    })();
 
 
     // const browser = await puppeteer.launch()
@@ -75,4 +34,44 @@ export default async function handler( req: NextApiRequest,res: NextApiResponse<
     // await browser.close()
 
     // return res.setHeader("content-type", "application/pdf").send(pdf)
+
+    try {
+        const browser = await playwright.chromium.launch({
+          args: [...chromium.args, "--font-render-hinting=none"], // This way fix rendering issues with specific fonts
+          executablePath:
+            process.env.NODE_ENV === "production"
+              ? await chromium.executablePath
+              : "/usr/local/bin/chromium",
+          headless:
+            process.env.NODE_ENV === "production" ? chromium.headless : true,
+        });
+    
+        const context = await browser.newContext();
+    
+        const page = await context.newPage();
+    
+        // This is the path of the url which shall be converted to a pdf file
+        const pdfUrl =
+          process.env.NODE_ENV === "production"
+            ? "https://your.app/pdf"
+            : "http://localhost:3000/pdf";
+    
+        await page.goto(`http://${host}/orderPdf?id=${id}`, {
+          waitUntil: "load",
+        });
+    
+        const pdf = await page.pdf({
+          path: "/tmp/awesome.pdf", // we need to move the pdf to the tmp folder otherwise it won't work properly
+          printBackground: true,
+          format: "a4",
+        });
+        await browser.close();
+    
+        return res.status(200).json({ pdf });
+
+      } catch (error) {
+        return res.status(500).json({ error: error });
+      }
+
+
 }
