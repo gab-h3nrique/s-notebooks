55c66edff73a1b37945d672b83a4503d41d6d0d8
gab Henrique <gabriel.aquino@camptecnologia.com.br>
2022-12-29 10:03:18 -0300

    aplicando correção

--- src/types/orderType.ts
+++ src/types/orderType.ts
@@ -10,20 +10,25 @@ export interface OrderType {
     brand:string;
     name:string;
     serialNumber:string;
+
     charger:boolean;
     battery:boolean;
     energyCable:boolean;
     bag:boolean;
-    others:string;
-    warranty:boolean;
-    warrantyDescription:string;
-    backup:boolean;
-    backupDescription:string;
-    defectDescription:string;
-    technicalReport:string;
-    equipamentPassword:string;
+    others?:string;
 
-    generalDescription:string;
+    warranty:boolean;
+    warrantyDescription?:string;
+
+    backup:boolean;
+    backupDescription?:string;
+
+    defectDescription?:string;
+    technicalReport?:string;
+    equipamentPassword?:string;
+    generalDescription?:string;
+
     deliveryConfirmation:boolean;
-    value:number;
+
+    value?:number;
 }
